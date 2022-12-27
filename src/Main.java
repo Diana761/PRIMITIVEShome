cf4d5081e2f4fe35fba31f69f95896a9e1a1434c
dianamilova <milova.diana.94.94@mail.ru>
2022-12-27 10:24:17 +0700

    change

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         int amount = 760;
-        int everyTwenty =20;
-        System.out.println ("начислено :");
-        System.out.println ((amount/everyTwenty) + "миль" );
+        int everyTwenty = 20;
+
+        System.out.println("начислено :");
+        System.out.println((amount / everyTwenty) + "миль");
     }
 }
