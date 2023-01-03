f5615a7e26a9a298b35796eef0e7e8516338965d
dianamilova <milova.diana.94.94@mail.ru>
2023-01-03 16:32:25 +0700

    new file

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int price = cost;
+        int bonus = 20;
+        int miles = price / bonus * 1;
+
+        return miles;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int amount = 760;
-        int everyTwenty = 20;
-
-        System.out.println("начислено :");
-        System.out.println((amount / everyTwenty) + "миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 760;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
